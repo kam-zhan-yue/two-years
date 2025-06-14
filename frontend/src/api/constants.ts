a26edaf624686653d8841af7f426cd48e1a352f3
Alex Kam <kamzhanyue@gmail.com>
2025-06-15 02:26:23 +0900

    chore: change deployed backend url

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -1,7 +1,7 @@
 const ENV = import.meta.env.MODE; // 'development' or 'production'
 
 export const BASE_URL = "http://0.0.0.0:8000/";
-const DEPLOYED_URL = "";
+const DEPLOYED_URL = "https://two-years-backend.onrender.com/";
 
 export const PRODUCTION = ENV === "production";
 export const API_URL = PRODUCTION ? DEPLOYED_URL : BASE_URL;
